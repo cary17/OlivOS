03a19f684fc3f703d106c118f0e9fb6d580468cc
cary17 <88092797+cary17@users.noreply.github.com>
2026-04-09 03:27:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 if [ -n "$EXTRA_PACKAGES" ]; then
-    pip3 install --no-cache-dir --break-system-packages \
-        --target=/app/deps $EXTRA_PACKAGES
+    pip install --no-cache-dir $EXTRA_PACKAGES
 fi
 
 cd /app/OlivOS
-exec python3 main.py "$@"
+exec python main.py "$@"
